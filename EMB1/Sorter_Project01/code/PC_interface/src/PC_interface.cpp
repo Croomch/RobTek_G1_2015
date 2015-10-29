23db2d0ef6a3a1ea506ec46721e708eba4815801
Croomch <lschw12@student.sdu.dk>
2015-10-29 18:03:12 +0100

    pc to fpga program

--- EMB1/Sorter_Project01/code/PC_interface/src/PC_interface.cpp
+++ EMB1/Sorter_Project01/code/PC_interface/src/PC_interface.cpp
@@ -9,107 +9,108 @@ using namespace std;
 
 
 //Function to write data in FPGA
-void write_FPGA(string portname, string command)
+void write_FPGA(asio::serial_port &port, string &command)
 {
-	asio::io_service io;
-	asio::serial_port port( io );
 
-	port.open(portname);
-	port.set_option(asio::serial_port_base::baud_rate(115200));
-
-	//Write the command to access to memory
-	asio::write(port, asio::buffer(command.c_str(), command.size()));
-
-	string command2;	
-	cout << "Set input data:" << endl;
-	cin >> command2;
+  //Write the command to access to memory
+  asio::write(port, asio::buffer(command.c_str(), command.size()));
 
 
-	asio::write(port, asio::buffer(command2.c_str(), command2.size()));
-
-	port.close();
+  cout << "Enter data:\n";
+  string cmd;
+  cin >> cmd;
+  asio::write(port, asio::buffer(cmd.c_str(), cmd.size()));
 
 }
 
 
 //Function to read data in FPGA
-string read_FPGA(string portname, string command)
-{	
-	asio::io_service io;
-	asio::serial_port port( io );
+void read_FPGA(asio::serial_port &port, string &command)
+{
 
-	port.open(portname);
-	port.set_option(asio::serial_port_base::baud_rate(115200));
-	
-	asio::mutable_buffer read_buf;
+  asio::mutable_buffer read_buf;
 
-	//Write the command to access to memory
-	asio::write(port, asio::buffer(command.c_str(), command.size()));
+  //Write the command to access to memory
+  asio::write(port, asio::buffer(command.c_str(), command.size()));
 
-	//Read the memory set and save it in char*
-	asio::read(port, asio::buffer(read_buf));
-	unsigned char* new_data = boost::asio::buffer_cast<unsigned char*>(read_buf);
+  char data[8] = {};
+  //Read the memory set and save it in char*
+  asio::read(port, asio::buffer(data));
+  //unsigned char* new_data = boost::asio::buffer_cast<unsigned char*>(read_buf);
 
-	string new_data_str((char*) new_data);
-	
-	port.close();
+  //string new_data_str((char*) new_data);
+  cout << "Data recieved was: " << data << "\n";
 
-	return new_data_str;
-
+  //return data;
 }
 
 
 int main()
 {
-	string command = "r01";
-	string portname;
-	string data_;
-
-	asio::mutable_buffer read_buf; 
-
-	//asio::const_buffer command_buf = asio::buffer(command);
-	//asio::const_buffers_1 command_buf;
-	
-//	asio::io_service io;
-//	asio::serial_port port( io );
+  string command = "r01";
+  string portname;
+  string data_;
 
 
 
-///////////////////////////////////////////////////////////////////////////////////////
-// 	Port Configuration
+  //	asio::mutable_buffer read_buf;
 
-	cout << "Set portname: " << endl;
-	cin >> portname;
+  //asio::const_buffer command_buf = asio::buffer(command);
+  //asio::const_buffers_1 command_buf;
 
-
-//////////////////////////////////////////////////////////////////////////////////////
-// 	While Program, each time ask for a new command
-	while(1){
-
-	cout << "\rSelect command:" << endl;
-	cin >> command;
-	
-
-	if(command[0] = 'r')
-		data_ = read_FPGA(portname, command);
-
-	if(command[0] = 'w')
-		write_FPGA(portname, command);
-
-	}
+  //	asio::io_service io;
+  //	asio::serial_port port( io );
 
 
 
-///////////////////////////////////////////////////////////////////////////////////////
-// 	Test read on buffer
-/*
+  ///////////////////////////////////////////////////////////////////////////////////////
+  // 	Port Configuration
 
-	asio::write(port, asio::buffer(command.c_str(), command.size()));
+  cout << "Set portname: " << endl;
+  cin >> portname;
 
-	asio::read(port, asio::buffer(read_buf));
-	unsigned char* p2 = boost::asio::buffer_cast<unsigned char*>(read_buf);
 
-	cout << p2;
+  asio::io_service io;
+  asio::serial_port port( io );
+
+  port.open(portname);
+  port.set_option(asio::serial_port_base::baud_rate(115200));
+  port.set_option(asio::serial_port_base::flow_control(asio::serial_port_base::flow_control::none));
+  port.set_option(asio::serial_port_base::parity(asio::serial_port_base::parity::none));
+  port.set_option(asio::serial_port_base::stop_bits(asio::serial_port_base::stop_bits::one));
+  port.set_option(asio::serial_port_base::character_size(8));
+
+  //////////////////////////////////////////////////////////////////////////////////////
+  // 	While Program, each time ask for a new command
+  while(1){
+
+      cout << "\rSelect command:" << endl;
+      cin >> command;
+
+
+      if(command[0] == 'r'){
+          cout << "Reading from port.\n";
+          read_FPGA(port, command);
+        } else if(command[0] == 'w'){
+          cout << "Writing to port.\n";
+          write_FPGA(port, command);
+        }
+
+    }
+
+
+  port.close();
+
+  ///////////////////////////////////////////////////////////////////////////////////////
+  // 	Test read on buffer
+  /*
+
+        asio::write(port, asio::buffer(command.c_str(), command.size()));
+
+        asio::read(port, asio::buffer(read_buf));
+        unsigned char* p2 = boost::asio::buffer_cast<unsigned char*>(read_buf);
+
+        cout << p2;
 */
 
 
