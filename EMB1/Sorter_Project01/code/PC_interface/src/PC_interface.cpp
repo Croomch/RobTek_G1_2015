3c7d57e91316942714c40f1ef5ec9a3668c7efff
gimait <aimig15@student.sdu.dk>
2015-10-29 17:16:04 +0100

    Updated PC_interface

--- EMB1/Sorter_Project01/code/PC_interface/src/PC_interface.cpp
+++ EMB1/Sorter_Project01/code/PC_interface/src/PC_interface.cpp
@@ -7,17 +7,39 @@ using namespace boost;
 using namespace std;
 
 
-/*
+
 //Function to write data in FPGA
-void write_FPGA(string command)
+void write_FPGA(string portname, string command)
 {
+	asio::io_service io;
+	asio::serial_port port( io );
+
+	port.open(portname);
+	port.set_option(asio::serial_port_base::baud_rate(115200));
+
+	//Write the command to access to memory
+	asio::write(port, asio::buffer(command.c_str(), command.size()));
+
+	string command2;	
+	cout << "Set input data:" << endl;
+	cin >> command2;
+
+
+	asio::write(port, asio::buffer(command2.c_str(), command2.size()));
+
+	port.close();
 
 }
 
 
 //Function to read data in FPGA
-string read_FPGA(string command)
+string read_FPGA(string portname, string command)
 {	
+	asio::io_service io;
+	asio::serial_port port( io );
+
+	port.open(portname);
+	port.set_option(asio::serial_port_base::baud_rate(115200));
 	
 	asio::mutable_buffer read_buf;
 
@@ -30,10 +52,12 @@ string read_FPGA(string command)
 
 	string new_data_str((char*) new_data);
 	
+	port.close();
+
 	return new_data_str;
 
 }
-*/
+
 
 int main()
 {
@@ -46,8 +70,8 @@ int main()
 	//asio::const_buffer command_buf = asio::buffer(command);
 	//asio::const_buffers_1 command_buf;
 	
-	asio::io_service io;
-	asio::serial_port port( io );
+//	asio::io_service io;
+//	asio::serial_port port( io );
 
 
 
@@ -65,41 +89,12 @@ int main()
 	cout << "\rSelect command:" << endl;
 	cin >> command;
 	
-	port.open(portname);
-	port.set_option(asio::serial_port_base::baud_rate(115200));
-	
+
 	if(command[0] = 'r')
-{
-//		data_ = read_FPGA(command);
-	
-		asio::mutable_buffer read_buf;
+		data_ = read_FPGA(portname, command);
 
-		//Write the command to access to memory
-		asio::write(port, asio::buffer(command.c_str(), command.size()));
-
-		//Read the memory set and save it in char*
-		asio::read(port, asio::buffer(read_buf));
-		unsigned char* new_data = boost::asio::buffer_cast<unsigned char*>(read_buf);
-
-		string new_data_str((char*) new_data);
-
-}
 	if(command[0] = 'w')
-{
-//		write_FPGA(command);
-		//Write the command to access to memory
-		asio::write(port, asio::buffer(command.c_str(), command.size()));
-
-		string command2;	
-		cout << "Set input data:" << endl;
-		cin >> command2;
-
-
-		asio::write(port, asio::buffer(command2.c_str(), command2.size()));
-
-}
-
-	port.close();
+		write_FPGA(portname, command);
 
 	}
 
