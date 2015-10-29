89032d5f6897a9008b4b8f61dcbec08055d9bc62
gimait <aimig15@student.sdu.dk>
2015-10-29 22:02:21 +0100

    Added code to read constantly the FPGA and print it on screen

---
+++ EMB1/Sorter_Project01/code/PC_print_colors/src/CMakeFiles/PC_print_colors.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PC_print_colors.dir/PC_print_colors.cpp.o"
+  "PC_print_colors.pdb"
+  "PC_print_colors"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PC_print_colors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
