eeb0a997671fb0e09b1a967d1535cea02e1d4acb
gimait <aimig15@student.sdu.dk>
2015-10-29 17:06:12 +0100

    PC_interface program added

---
+++ EMB1/Sorter_Project01/code/PC_interface/src/CMakeFiles/PC_interface.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PC_interface.dir/PC_interface.cpp.o"
+  "PC_interface.pdb"
+  "PC_interface"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PC_interface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ EMB1/Sorter_Project01/code/PC_interface/src/PC_interface.cpp
@@ -0,0 +1,122 @@
+
+#include <boost/asio.hpp>
+#include <iostream>
+#include <string>
+
+using namespace boost;
+using namespace std;
+
+
+/*
+//Function to write data in FPGA
+void write_FPGA(string command)
+{
+
+}
+
+
+//Function to read data in FPGA
+string read_FPGA(string command)
+{	
+	
+	asio::mutable_buffer read_buf;
+
+	//Write the command to access to memory
+	asio::write(port, asio::buffer(command.c_str(), command.size()));
+
+	//Read the memory set and save it in char*
+	asio::read(port, asio::buffer(read_buf));
+	unsigned char* new_data = boost::asio::buffer_cast<unsigned char*>(read_buf);
+
+	string new_data_str((char*) new_data);
+	
+	return new_data_str;
+
+}
+*/
+
+int main()
+{
+	string command = "r01";
+	string portname;
+	string data_;
+
+	asio::mutable_buffer read_buf; 
+
+	//asio::const_buffer command_buf = asio::buffer(command);
+	//asio::const_buffers_1 command_buf;
+	
+	asio::io_service io;
+	asio::serial_port port( io );
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+// 	Port Configuration
+
+	cout << "Set portname";
+	cin >> portname;
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+// 	While Program, each time ask for a new command
+	while(1){
+
+	cout << "\rSelect command:" << endl;
+	cin >> command;
+	
+	port.open(portname);
+	port.set_option(asio::serial_port_base::baud_rate(115200));
+	
+	if(command[0] = 'r')
+{
+//		data_ = read_FPGA(command);
+	
+		asio::mutable_buffer read_buf;
+
+		//Write the command to access to memory
+		asio::write(port, asio::buffer(command.c_str(), command.size()));
+
+		//Read the memory set and save it in char*
+		asio::read(port, asio::buffer(read_buf));
+		unsigned char* new_data = boost::asio::buffer_cast<unsigned char*>(read_buf);
+
+		string new_data_str((char*) new_data);
+
+}
+	if(command[0] = 'w')
+{
+//		write_FPGA(command);
+		//Write the command to access to memory
+		asio::write(port, asio::buffer(command.c_str(), command.size()));
+
+		string command2;	
+		cout << "Set input data:" << endl;
+		cin >> command2;
+
+
+		asio::write(port, asio::buffer(command2.c_str(), command2.size()));
+
+}
+
+	port.close();
+
+	}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+// 	Test read on buffer
+/*
+
+	asio::write(port, asio::buffer(command.c_str(), command.size()));
+
+	asio::read(port, asio::buffer(read_buf));
+	unsigned char* p2 = boost::asio::buffer_cast<unsigned char*>(read_buf);
+
+	cout << p2;
+*/
+
+
+
+}
