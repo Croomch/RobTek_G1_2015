1a4e2679fccd8cabba526185c8b0d822e0e95701
gimait <aimig15@student.sdu.dk>
2015-11-16 14:23:26 +0100

    actualized Communication.vhd

--- EMB1/FinalProject/Code/segway.srcs/sources_1/new/Communication.vhd
+++ EMB1/FinalProject/Code/segway.srcs/sources_1/new/Communication.vhd
@@ -38,31 +38,28 @@ architecture Behavioral of Communication is
 
 signal counter: integer := 0;
 
-<<<<<<< HEAD
 signal BAUD_CLK: STD_LOGIC := "0";
 signal inputSignal: STD_LOGIC_VECTOR := "111111111";
 signal com : STD_LOGIC := "0";
-=======
 signal BAUD_CLK: STD_LOGIC := '0';
 signal inputSignal: STD_LOGIC_VECTOR(7 downto 0) := "111111111";
->>>>>>> 31033ba0044eb81c355ca361aa274ee98456c526
 
 begin
 
 -- BAUD GENERATION
 -- clk scaler to get 115200 bps == 115200 Hz
-BAUD_CLK <= BAUD_SPI;
-process(CLK)
-variable scaler : integer range 0 to CLK_SCALING/2 := 0;
-begin
-    if rising_edge(CLK) then
-        scaler := scaler + 1;
-        if scaler >= CLK_SCALING/2 then 
-            scaler := 0;
-            CLK_SPI <= NOT(CLK_SPI);
-        end if;
-    end if;
-end process;
+--BAUD_CLK <= BAUD_SPI;
+--process(CLK)
+--variable scaler : integer range 0 to CLK_SCALING/2 := 0;
+--begin
+ --   if rising_edge(CLK) then
+ --       scaler := scaler + 1;
+ --       if scaler >= CLK_SCALING/2 then 
+ --           scaler := 0;
+ --           CLK_SPI <= NOT(CLK_SPI);
+ --       end if;
+ --   end if;
+--end process;
 
 
 --Syncronize?
@@ -73,19 +70,11 @@ end process;
 
 process(CLK, com)
 begin
-<<<<<<< HEAD
     if (rising_edge(CLK) and comin = 0) then
         inputSignal <= inputSignal(8 downto 1) & RX;
         if (inputSignal < "111111100") then
             com <= 1;
         end if;        
-=======
-    if (rising_edge(CLK) and com = 0) then
-        inputSignal <= inputSignal(7 downto 1) & RX;
-        if inputSignal < "111111100" then
-        
-        end if;
->>>>>>> 31033ba0044eb81c355ca361aa274ee98456c526
     end if;
 
 end process;
@@ -95,6 +84,7 @@ end process;
 -- when reading finish -> communication off
 
 process(BAUD_CLK,com)
+begin
     if (rising_edge(BAUD_CLK) and com = 1) then 
         instructions <= instructions(7 downto 0) & RX;
         bitcounter <= bitcounter + "1";
