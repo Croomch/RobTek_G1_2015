4dc276966ebe9327c520b079f9170ae361e75e09
Croomch <lschw12@student.sdu.dk>
2015-11-19 10:20:10 +0100

    ignore updated

--- EMB1/FinalProject/Code/segway.srcs/sources_1/new/topmodule.vhd
+++ EMB1/FinalProject/Code/segway.srcs/sources_1/new/topmodule.vhd
@@ -21,6 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -42,7 +44,12 @@ entity topmodule is
     SPI_CLK : out STD_LOGIC;
     SPI_CS : out STD_LOGIC;
     
-    ALIVE : out STD_LOGIC
+    ALIVE : out STD_LOGIC;
+    
+    XB_SERIAL_O   		: out	STD_LOGIC;                       -- Serial stream to PC
+    XB_SERIAL_I	   	: in	STD_LOGIC;                       -- Serial stream from PC
+    XB_LEDS_O			: out	STD_LOGIC_VECTOR(2 downto 0);    -- 3 LED's on expansion board
+    XB_PSW_I        : in  STD_LOGIC_VECTOR(3 downto 0)    -- 4 dip switches
     );
 end topmodule;
 
@@ -52,26 +59,35 @@ architecture Behavioral of topmodule is
 signal L_FWD, L_BACK, R_FWD, R_BACK : STD_LOGIC_VECTOR(7 downto 0) := "00000000";
 signal L_FWD_PWM, L_BACK_PWM, R_FWD_PWM, R_BACK_PWM : STD_LOGIC := '0';
 signal CLK_SLOW : STD_LOGIC := '0';
+-- SPI data --
+signal spi_rx : STD_LOGIC_VECTOR(7 downto 0) := "00000000";
+signal spi_rx_sig : STD_LOGIC := '0';
+signal spi_tx_sig : STD_LOGIC := '0';
+signal spi_tx : STD_LOGIC_VECTOR(7 downto 0) := "00000000";
+-- alive signal --
+signal ALIVE_LED : STD_LOGIC := '0';
+
 ---- CONSTANTS ----
-CONSTANT CLK_SCALING : INTEGER := 10;
+CONSTANT CLK_FREQ : INTEGER := 50000000;
+CONSTANT CLK_SCALING : INTEGER := 10; -- for the generation of clk signal for the motors
+CONSTANT ALIVE_PERIOD : INTEGER := CLK_FREQ;
 
 ---- COMPONENTS ----
-COMPONENT SPI IS
-Port ( 
-           CLK : in STD_LOGIC;
-    
-           SPI_CLK : out STD_LOGIC;
-           SPI_MOSI : out STD_LOGIC;
-           SPI_MISO : in STD_LOGIC;
-           SPI_CS : out STD_LOGIC;
-           
-           output : out STD_LOGIC_VECTOR (9 downto 0);
-           output_updated : out STD_LOGIC;
-           
-           getSample : in STD_LOGIC;
-           SPI_MSG : in STD_LOGIC_VECTOR (3 downto 0)
-           );
-END COMPONENT;
+-- Signals below is used to connect to the Pseudo TosNet Controller component  
+signal T_reg_ptr                 : std_logic_vector(2 downto 0);
+signal T_word_ptr                : std_logic_vector(1 downto 0);
+signal T_data_to_mem             : std_logic_vector(31 downto 0);
+signal T_data_from_mem           : std_logic_vector(31 downto 0);
+signal T_data_from_mem_latch     : std_logic;
+
+-- Here we define the signals used by the top level design
+signal sys_cnt				: std_logic_vector(31 downto 0) := (others => '0');
+signal freq_gen          : std_logic_vector(31 downto 0) := (others => '0');
+signal freq_out          : std_logic := '0';
+signal bb_leds				: std_logic_vector(7 downto 0);  -- register for 8 leds
+signal dipsw             : std_logic_vector(3 downto 0);
+signal frq,flsh      : std_logic;
+
 
 COMPONENT motorcontrol IS
 Port ( 
@@ -82,37 +98,94 @@ Port (
            );
 END COMPONENT;
 
+-- Here we define the components we want to include in our design (there is only one)
+-- The Port description is just copied from the components own source file
+COMPONENT PseudoTosNet_ctrl is
+Port (
+    T_clk_50M							: in	STD_LOGIC;
+    T_serial_out						: out STD_LOGIC;
+    T_serial_in                   : in  STD_LOGIC;
+    T_reg_ptr							: out std_logic_vector(2 downto 0);
+    T_word_ptr							: out std_logic_vector(1 downto 0);
+    T_data_to_mem						: in  std_logic_vector(31 downto 0);
+    T_data_from_mem					: out std_logic_vector(31 downto 0);
+    T_data_from_mem_latch			: out std_logic
+    );
+END COMPONENT;
+
+-- spi with gyro/acc --
+COMPONENT SPI IS
+    Port (  CLK : in STD_LOGIC;
+    
+           SPI_CLK : out STD_LOGIC;
+           SPI_MOSI : out STD_LOGIC;
+           SPI_MISO : in STD_LOGIC;
+           SPI_CS : out STD_LOGIC;
+           
+           output : out STD_LOGIC_VECTOR (7 downto 0);
+           output_updated : out STD_LOGIC;
+           
+           getSample : in STD_LOGIC;
+           SPI_MSG : in STD_LOGIC_VECTOR (7 downto 0)
+            );
+END COMPONENT;
+
 begin
 
+-- Here we instantiate the Pseudo TosNet Controller component, and connect it's ports to signals	
+PseudoTosNet_ctrlInst : PseudoTosNet_ctrl
+Port map (
+    T_clk_50M          => CLK,
+	T_serial_out       => XB_SERIAL_O,
+	T_serial_in        => XB_SERIAL_I,
+	T_reg_ptr		     => T_reg_ptr,					
+	T_word_ptr		     => T_word_ptr,									
+	T_data_to_mem	     => T_data_to_mem,					
+	T_data_from_mem	     => T_data_from_mem,						
+	T_data_from_mem_latch => T_data_from_mem_latch
+);
+
 -- init the components needed
-MOTOR_CONTROL(1) <= L_FWD_PWM;
-MOTOR_CONTROL(2) <= L_BACK_PWM;
-MOTOR_CONTROL(3) <= R_FWD_PWM;
-MOTOR_CONTROL(4) <= R_BACK_PWM;
+MOTOR_CONTROL(0) <= L_FWD_PWM;
+MOTOR_CONTROL(1) <= L_BACK_PWM;
+MOTOR_CONTROL(2) <= R_FWD_PWM;
+MOTOR_CONTROL(3) <= R_BACK_PWM;
+MOTOR_CONTROL(7 downto 4) <= "0000";
 L_F : motorcontrol Port map (
-CLK => CLK_SLOW,
-PWM => L_FWD_PWM,
-duty => L_FWD
+    CLK => CLK_SLOW,
+    PWM => L_FWD_PWM,
+    duty => L_FWD
 );
 
 L_B : motorcontrol Port map (
-CLK => CLK_SLOW,
-PWM => L_BACK_PWM,
-duty => L_BACK
+    CLK => CLK_SLOW,
+    PWM => L_BACK_PWM,
+    duty => L_BACK
 );
 
 R_F : motorcontrol Port map (
-CLK => CLK_SLOW,
-PWM => R_FWD_PWM,
-duty => R_FWD
+    CLK => CLK_SLOW,
+    PWM => R_FWD_PWM,
+    duty => R_FWD
 );
 
 R_B : motorcontrol Port map (
-CLK => CLK_SLOW,
-PWM => R_BACK_PWM,
-duty => R_BACK
+    CLK => CLK_SLOW,
+    PWM => R_BACK_PWM,
+    duty => R_BACK
 );
 
+spi_c : SPI Port map (
+    CLK => CLK,
+    SPI_CLK => SPI_CLK,
+    SPI_MOSI => SPI_MOSI,
+    SPI_MISO => SPI_MISO,
+    SPI_CS => SPI_CS,
+    output => spi_rx,
+    output_updated => spi_rx_sig,
+    getSample => spi_tx_sig,
+    SPI_MSG => spi_tx
+);
 
 ---- clk scaler for the pwm generators ----
 process(CLK)
@@ -138,6 +211,58 @@ end process;
 --y_c = x_c * 0.375 + 0.42
 
 
+-- alive timer --
+-- generate a regular blinking on the onboard led 
+ALIVE <= ALIVE_LED;
+process(CLK)
+variable alive_scaler : integer range 0 to ALIVE_PERIOD/2 := 0;
+begin
+    if rising_edge(CLK) then
+        alive_scaler := alive_scaler + 1;
+        if alive_scaler >= ALIVE_PERIOD/2 then 
+            alive_scaler := 0;
+            ALIVE_LED <= NOT(ALIVE_LED);
+        end if;
+    end if;
+    
+end process;
 
+
+---------------------------------------------------------
+-- Clocked process, to take data off the controller bus	
+----------------------------------------------------------
+  DatFromTosNet: 	
+  process(CLK)
+  begin -- process
+    if (CLK'event and CLK='1' and T_data_from_mem_latch='1') then
+	   case (T_reg_ptr & T_word_ptr) is                        -- The addresses are concatenated for compact code
+--		  when "00000" => period    <= T_data_from_mem;               -- Register 0, word 0 - all 32 bits
+--		  when "00001" => pwm_value <= T_data_from_mem(15 downto 0);  -- Register 0, word 1 - low 16 bits
+--		                  flash     <= T_data_from_mem(31 downto 24); --                      high 8 bits
+--		  when "00100" => v_leds    <= T_data_from_mem;               -- Register 1, word 0 - all 32 bits
+          -- others
+		  when others =>
+		end case;
+	 end if;
+  end process;
+
+----------------------------------------------------------
+-- Unclocked process, to place data on the controller bus
+----------------------------------------------------------
+   DatToTosNet:
+	process(T_reg_ptr,T_word_ptr)
+	begin
+		T_data_to_mem<="00000000000000000000000000000000";	-- default data
+		case (T_reg_ptr & T_word_ptr) is                   -- The addresses are concatenated for compact code
+		   -- Register 0, word 0-3 are hard coded to these values for test/demo purposes
+			when "00000" =>	T_data_to_mem <= "00000000000000000000000000000001"; -- 1
+			when "00001" =>	T_data_to_mem <= "00000000000000000000000000000010"; -- 2
+			when "00010" =>   T_data_to_mem <= "00000000000000000000000000000100"; -- 3
+			when "00011" => 	T_data_to_mem <= "00000000000000000000000000001000"; -- 4
+         -- Register 1
+--       Etc. etc. etc.
+			when others =>
+		end case;		
+	end process;
 
 end Behavioral;
